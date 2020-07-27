5314b4bb7f796bd67fd6b7bf72961c55e6510459
You Song Shan <songshan@lionsbot.com>
2020-07-27 17:26:46 +0800

    Deproject free space into 3d pointcloud & clean code

--- scripts/inference.py
+++ scripts/inference.py
@@ -19,10 +19,12 @@ class Config(object):
         self.infer_size = (720, 960, 3)
         self.is_training = is_training
         self.filter_scale = filter_scale
+        self.camera_info = None
 
 class ICNetInference:
     
     def __init__(self, weight_path):
+
         self.cfg = Config()
         self.cfg.model_weight = weight_path
         self.net = ICNet_BN(cfg=self.cfg, mode='inference')
@@ -30,37 +32,41 @@ class ICNetInference:
         self.net.restore(self.cfg.model_weight)
 
     def infer(self, image):
+
         dim = (self.cfg.param['infer_size'][1], self.cfg.param['infer_size'][0])
         image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
 
         start_t = time.time()
         result = self.net.predict(image)
         duration = time.time() - start_t
-        
+
         return result, duration
 
-    def process(self, image, result, detection_duration, alpha=0.3):
+    def process(self, image, result, infer_duration, alpha=0.3):
+
         start_t = time.time()
         dim = (self.cfg.param['infer_size'][1], self.cfg.param['infer_size'][0])
         image = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
-        
         overlay = (0.5 * image + 0.5 * result).astype("uint8")
-
         result = cv2.cvtColor(result, cv2.COLOR_RGB2GRAY)/255.0
         result = (abs(result-1))
         result = np.uint8(result)
-        output, boundary = self.extract_boundary_np(overlay, result)
-        process_duration = time.time() - start_t
-        output = self.render_image(output, detection_duration, process_duration)
 
-        return output, boundary
+        output, boundary = self.extract_boundary_np(overlay, result)
+        if self.camera_info is not None:
+            points = self.deproject_points(boundary, self.camera_info)
+        process_duration = time.time() - start_t
+
+        output = self.render_image(output, infer_duration, process_duration)
+
+        return output, points
 
     def extract_boundary_np(self, image, result):
+
         result[0:int(0.45*result.shape[0]),:] = 1
         mask = cv2.copyMakeBorder(result, 0, 1, 0, 0, cv2.BORDER_CONSTANT, None, [0,0,0])
         edge = mask[:-1,:] - mask[1:,:]
-        points = np.nonzero(edge)
-        points = np.transpose(points)
+        points = np.transpose(np.nonzero(edge))
         idx = np.lexsort((-points[:,0],points[:,1]))  
         sorted_idx = points[idx]
         
@@ -68,17 +74,18 @@ class ICNetInference:
         unique_idx = sorted_idx[unique_idx]
         
         for p in unique_idx:
-            image_draw = cv2.circle(image, (p[1],p[0]), 1, (255, 255, 0) , 5)
+            cv2.circle(image, (p[1],p[0]), 1, (255, 255, 0) , 5)
 
-        return image_draw, unique_idx
+        return image, unique_idx
 
-    def render_image(self, image, detection_duration, process_duration):
-        detection_fps = 1.0 / detection_duration
-        total_time = detection_duration + process_duration
+    def render_image(self, image, infer_duration, process_duration):
+
+        infer_fps = 1.0 / infer_duration
+        total_time = infer_duration + process_duration
         total_fps = 1.0 / total_time
 
-        text1 = "Detection time: {:.2f} ms,   Detection FPS: {:.2f}".format(detection_duration*1000, detection_fps)
-        text2 = "Total time:     {:.2f} ms,       Total FPS: {:.2f}".format(total_time*1000, total_fps)
+        text1 = "Inference time: {:.2f} ms,   Inference FPS: {:.2f}".format(infer_duration*1000, infer_fps)
+        text2 = "    Total time: {:.2f} ms,       Total FPS: {:.2f}".format(total_time*1000, total_fps)
         org1 = (int(image.shape[0]*0.05), int(image.shape[1]*0.05))
         org2 = (int(image.shape[0]*0.05), int(image.shape[1]*0.10))
 
@@ -86,3 +93,48 @@ class ICNetInference:
         cv2.putText(image, text2, org2, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 255), 2, cv2.LINE_AA)
 
         return image
+
+    def deproject_points(self, image_points, camera_info):
+
+        scale_u = self.cfg.param['infer_size'][0] / float(camera_info.height)
+        scale_v = self.cfg.param['infer_size'][1] / float(camera_info.width)
+        S = np.array([ [scale_u, 0, 0], 
+                       [0, scale_v, 0],
+                       [0,       0, 1] ])
+        K = np.array(camera_info.K).reshape((3,3))
+        R = np.array(camera_info.R).reshape((3,3))
+        P = np.array(camera_info.P).reshape((3,4))
+
+        ####### Hardcode intrinsic matrix and z value
+        K[0] = P[0][:-1]
+        K[1] = P[1][:-1]
+        K[2] = P[2][:-1]
+        K[0][2] = 1080 / 2.18
+        K[1][2] = 1440 / 2
+
+        z = - 0.875
+        #######
+        K_scaled = np.dot(S, K)
+        KI_scaled = np.linalg.inv(K_scaled)
+
+        world_points = []
+
+        for point in image_points:
+            if point[0] == self.cfg.param['infer_size'][0]-1:
+                continue
+            image_pt = np.transpose(np.array([point[0], point[1], 1]))
+            world_pt = np.dot(KI_scaled,image_pt)
+            x1 = world_pt[2]
+            y1 = - world_pt[1]
+            z1 = - world_pt[0]
+
+            t = z / z1
+            x = x1*t
+            y = y1*t
+
+            dist = np.sqrt(x*x + y*y)
+
+            if x > 0 and dist <= 8:
+                world_points.append([x,y,z])
+    
+        return world_points
--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import rospy
-from sensor_msgs.msg import Image
+from std_msgs.msg import Header
+from sensor_msgs.msg import Image, CameraInfo, PointCloud
+from geometry_msgs.msg import Point32
 from cv_bridge import CvBridge
 
 from inference import ICNetInference
@@ -9,35 +11,52 @@ class RosInference:
 
     def __init__(self):
         self.m_rate = rospy.get_param("~m_rate", 30.0)
+        self.m_input_camera_info_topic = rospy.get_param("~m_input_camera_info_topic", "/camera/camera_info")
         self.m_input_image_topic = rospy.get_param("~m_input_image_topic", "/camera/image_raw")
         self.m_output_image_topic = rospy.get_param("~m_output_image_topic", "/camera/segmentation")
+        self.m_output_pointcloud_topic = rospy.get_param("~m_output_pointcloud_topic", "/camera/pointcloud")
         self.m_weight_path = rospy.get_param("~m_weight_path", "/proj/tf_ws/src/ros_icnet/model/weight/model_best.ckpt")
         
         self.inference = ICNetInference(self.m_weight_path)
         self.bridge = CvBridge()
 
-        self.image_sub = rospy.Subscriber(self.m_input_image_topic, Image, self.image_callback)
+        self.cam_info_sub = rospy.Subscriber(self.m_input_camera_info_topic, CameraInfo, self.info_callback, queue_size = 1)
+        self.image_sub = rospy.Subscriber(self.m_input_image_topic, Image, self.image_callback, queue_size = 1)
+
         self.image_pub = rospy.Publisher(self.m_output_image_topic, Image, queue_size=1)
+        self.pointcloud_pub = rospy.Publisher(self.m_output_pointcloud_topic, PointCloud, queue_size=1)
 
         self.rate = rospy.Rate(self.m_rate)
-
         self.image = None
 
     def image_callback(self, msg):
         self.image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
 
-    def spin(self):
+    def info_callback(self, msg):
+        self.inference.camera_info = msg
 
+    def spin(self):
         while not rospy.is_shutdown():
 
             if self.image is not None:
                 result, duration = self.inference.infer(self.image)
-                output_image, boundary = self.inference.process(self.image, result[0], duration)
-                print(boundary)
-                #TODO: pointcloud = self.create_pointcloud(boundary)
+                output_image, points = self.inference.process(self.image, result[0], duration)
+
+                pointcloud = PointCloud()
+                header = Header()
+                header.stamp = rospy.Time.now()
+                header.frame_id = 'camera_link'
+                pointcloud.header = header
+
+                for point in points:
+                    pointcloud.points.append(Point32(point[0], point[1], point[2]))
 
                 output_image = self.bridge.cv2_to_imgmsg(output_image)
                 self.image_pub.publish(output_image)
+
+                self.pointcloud_pub.publish(pointcloud)
+
+                # self.image = None
 
             self.rate.sleep()
 
